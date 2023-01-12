29c8e11090cf8c3741d8db0708531c16c03b24b2
Newton Ombese <newtonombese1@gmail.com>
2023-01-12 08:54:53 +0300

    Task 0 - Minimum Operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''
+Given a number n, write a method that calculates
+the fewest number of operations needed to result in
+exactly n H characters in the file.
+'''
+
+
+def minOperations(n):
+    """minOperations is a method that calculates
+    the fewest number of operations needed to result
+    in exactly n H characters
+    Args:
+        n (int): amount of H
+    Return:
+        minimum number of operations (an integer)
+    """
+    minOp, div = 0, 2
+    while isinstance(n, int) and n > 1:
+        while n % div:
+            div += 1
+        minOp += div
+        n = int(n / div)
+    return 
